4b8a5e36bed746da4b4659d1a66118c1cd376c6d
Muhab <muhabme@gmail.com>
2023-06-15 08:00:45 +0300

    added get route for category

--- src/app/api/categories/[id]/route.js
+++ src/app/api/categories/[id]/route.js
@@ -1,4 +1,32 @@
 import { NextResponse } from 'next/server';
+import { Category } from '../../../../models/Category';
+
+export const GET = async (request) => {
+    const id = request.url.split('/categories/')[1];
+    try {
+        let categories = [];
+        let category = await Category.findOne({ _id: id });
+        categories.push(category);
+
+        while (category.parent) {
+            const newCategory = await Category.findOne({
+                _id: category.parent._id,
+            });
+            category = newCategory;
+            categories.push(newCategory);
+
+            if (!category.parent) {
+                break;
+            }
+        }
+
+        return new NextResponse(JSON.stringify(categories), {
+            status: 200,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export const DELETE = async (request) => {
     const id = request.url.split('/categories/')[1];
